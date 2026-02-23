ebcc266a4532f2970d0723e1a00e3bd42bae3d6e
rogeriosaj <rogeriosucuglia@gmail.com>
2026-02-23 16:37:25 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/date_options/',
+  base: '/surprise2/',
   server: {
     port: 5173,
     strictPort: true,
